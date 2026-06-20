b4a46ae62838473b23c03c8b74ea7b7c4a192e3c
swarn6402 <swarnjeettiwary01@gmail.com>
2026-06-20 13:40:10 +0530

    Add LocalMatchTime component for formatted time display

---
+++ src/components/ui/LocalMatchTime.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+interface LocalMatchTimeProps {
+  utcDate: string;
+  className?: string;
+}
+
+function formatLocalMatchTime(utcDate: string): string {
+  const date = new Date(utcDate);
+  if (Number.isNaN(date.getTime()) || typeof window === "undefined") {
+    return "";
+  }
+
+  return new Intl.DateTimeFormat(undefined, {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    timeZoneName: "short",
+  }).format(date);
+}
+
+export function LocalMatchTime({ utcDate, className }: LocalMatchTimeProps) {
+  return (
+    <time dateTime={utcDate} className={className} suppressHydrationWarning>
+      {formatLocalMatchTime(utcDate)}
+    </time>
+  );
+}
